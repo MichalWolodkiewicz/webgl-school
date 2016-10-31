7747f7f6633736b60268264659ac3944810d5e77
Michał Wołodkiewicz <michal.wolodkiewicz@contractors.roche.com>
2016-10-31 16:14:29 +0100

    chapter 4 - mouse events added to rotate circle.

--- www/gl_script.js
+++ www/gl_script.js
@@ -2,6 +2,9 @@ var GL;
 var logBox;
 var triangle_buffer;
 var faces_buffer;
+var canvas;
+var theta = 0,
+    phi = 0;
 
 function addColor(colors, circleVertexes) {
     for (var c = 0; c < colors.length; ++c) {
@@ -22,11 +25,10 @@ var colorGenerator = {
 
 function calculateCircleVertexes(angleStep) {
     var circleVertexes = [0, 0, 0, 1, 0, 0];
-    var radiansConverter = 180 / Math.PI;
     for (var i = 0; i <= 360; i += angleStep) {
-        console.log([i, Math.cos(i/180*Math.PI), Math.sin(i/180*Math.PI)]);
-        circleVertexes.push(Math.cos(i/180*Math.PI));
-        circleVertexes.push(Math.sin(i/180*Math.PI));
+        console.log([i, Math.cos(i / 180 * Math.PI), Math.sin(i / 180 * Math.PI)]);
+        circleVertexes.push(Math.cos(i / 180 * Math.PI));
+        circleVertexes.push(Math.sin(i / 180 * Math.PI));
         circleVertexes.push(0);
         addColor(colorGenerator.generate(), circleVertexes);
     }
@@ -35,7 +37,7 @@ function calculateCircleVertexes(angleStep) {
 
 function calculateCircleFaces() {
     var circleFaces = [];
-    for (var i = 6; i < circle_vertex.length-6; i += 6) {
+    for (var i = 6; i < circle_vertex.length - 6; i += 6) {
         circleFaces.push(0);
         circleFaces.push(i / 6);
         circleFaces.push(i / 6 + 1);
@@ -66,7 +68,8 @@ function initLogger() {
 
 function initWebGL() {
     initLogger();
-    var canvas = document.getElementById('glCanvas');
+    canvas = document.getElementById('glCanvas');
+    initMouseEvent();
     try {
         GL = canvas.getContext('webgl', {antialias: true}) || canvas.getContext('web-gl-academy-context', {antialias: true});
     } catch (e) {
@@ -83,7 +86,8 @@ function initWebGL() {
     var fragment_shader = get_shader(fragment_shader_src, GL.FRAGMENT_SHADER);
     var program = createProgram(vertex_shader, fragment_shader);
     var _Pmatrix = GL.getUniformLocation(program, "Pmatrix");
-    var _Mmatrix = GL.getUniformLocation(program, "Mmatrix");
+    var _MmatrixY = GL.getUniformLocation(program, "MmatrixY");
+    var _MmatrixX = GL.getUniformLocation(program, "MmatrixX");
     var _Vmatrix = GL.getUniformLocation(program, "Vmatrix");
     var _position = GL.getAttribLocation(program, 'position');
     var _color = GL.getAttribLocation(program, 'color');
@@ -94,32 +98,38 @@ function initWebGL() {
     faces_buffer = GL.createBuffer();
     bindTriangleBuffers();
     var PROJMATRIX = LIBS.get_projection(40, canvas.width / canvas.height, 1, 100);
-    var MOVEMATRIX = LIBS.get_I4();
+    var MOVEMATRIX_Y = LIBS.get_I4();
+    var MOVEMATRIX_X = LIBS.get_I4();
     var VIEWMATRIX = LIBS.get_I4();
     LIBS.translateZ(VIEWMATRIX, -5);
     GL.clearColor(0.0, 0.0, 0.0, 0.0);
     GL.enable(GL.DEPTH_TEST);
     GL.depthFunc(GL.LEQUAL);
     GL.clearDepth(1.0);
-    var time_old = 0;
-    var animate = function (time) {
-        var dAngle = 0.005 * (time - time_old);
-        LIBS.rotateY(MOVEMATRIX, dAngle);
-        time_old = time;
-        GL.viewport(0.0, 0.0, canvas.width, canvas.height);
+    GL.uniformMatrix4fv(_Pmatrix, false, PROJMATRIX);
+    GL.viewport(0.0, 0.0, canvas.width, canvas.height);
+    GL.vertexAttribPointer(_position, 3, GL.FLOAT, false, 4 * (3 + 3), 0);
+    GL.vertexAttribPointer(_color, 3, GL.FLOAT, false, 4 * (3 + 3), 3 * 4);
+    GL.uniformMatrix4fv(_Vmatrix, false, VIEWMATRIX);
+    var animate = function () {
+        if (!drag) {
+            dX *= amortization;
+            dY *= amortization;
+            theta += dX;
+            //phi += dY;
+        }
+        LIBS.set_I4(MOVEMATRIX_Y);
+        LIBS.set_I4(MOVEMATRIX_X);
+        LIBS.rotateY(MOVEMATRIX_Y, theta);
+        LIBS.rotateX(MOVEMATRIX_X, phi);
         GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
-        GL.uniformMatrix4fv(_Pmatrix, false, PROJMATRIX);
-        GL.uniformMatrix4fv(_Mmatrix, false, MOVEMATRIX);
-        GL.uniformMatrix4fv(_Vmatrix, false, VIEWMATRIX);
-        GL.bindBuffer(GL.ARRAY_BUFFER, triangle_buffer);
-        GL.vertexAttribPointer(_position, 3, GL.FLOAT, false, 4 * (3 + 3), 0);
-        GL.vertexAttribPointer(_color, 3, GL.FLOAT, false, 4 * (3 + 3), 3 * 4);
-        GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, faces_buffer);
+        GL.uniformMatrix4fv(_MmatrixY, false, MOVEMATRIX_Y);
+        GL.uniformMatrix4fv(_MmatrixX, false, MOVEMATRIX_X);
         GL.drawElements(GL.TRIANGLES, vertex_number, GL.UNSIGNED_SHORT, 0);
         GL.flush();
         window.requestAnimationFrame(animate);
     };
-    animate(0);
+    animate();
     return true;
 }
 
@@ -129,9 +139,10 @@ var vertex_shader_src = "\n\
     uniform mat4 Pmatrix;\n\
     uniform mat4 Vmatrix;\n\
     varying vec3 vColor;\n\
-    uniform mat4 Mmatrix;\n\
+    uniform mat4 MmatrixY;\n\
+    uniform mat4 MmatrixX;\n\
     void main(void) {\
-        gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);\
+        gl_Position = Pmatrix*Vmatrix*MmatrixY*MmatrixX*vec4(position, 1.);\
         vColor = color;\
     }";
 
@@ -171,5 +182,40 @@ var get_shader = function (source, type) {
     }
     logNormal(getShaderNameByType(type) + 'compiled');
     return shader;
+}
+
+// mouse events variables
+var drag = false;
+var old_x, old_y;
+var amortization = 0.95;
+var dX = 0.0, dY = 0.0;
+
+var mouseDown = function (e) {
+    drag = true;
+    old_x = e.pageX, old_y = e.pageY;
+    e.preventDefault();
+    return false;
+};
+
+var mouseUp = function (e) {
+    drag = false;
+};
+
+var mouseMove = function (e) {
+    if (!drag) return false;
+    dX = (e.pageX - old_x) * 2 * Math.PI / canvas.width;
+    dY = (e.pageY - old_y) * 2 * Math.PI / canvas.height;
+    theta += dX;
+    //phi+=dY;
+    old_x = e.pageX;
+    old_y = e.pageY;
+    e.preventDefault();
+};
+
+function initMouseEvent() {
+    canvas.addEventListener("mousedown", mouseDown, false);
+    canvas.addEventListener("mouseup", mouseUp, false);
+    canvas.addEventListener("mouseout", mouseUp, false);
+    canvas.addEventListener("mousemove", mouseMove, false);
 }
 
